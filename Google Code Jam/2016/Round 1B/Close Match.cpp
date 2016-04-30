cf86331e789fe00c722df020fdfea59851450ba5
tobygameac <tobyreallygameac@gmail.com>
2016-05-01 03:34:19 +0800

    2016/05/01

--- Google Code Jam/2016/Round 1B/Close Match.cpp
+++ Google Code Jam/2016/Round 1B/Close Match.cpp
@@ -6,26 +6,18 @@
 #include <string>
 #include <vector>
 
-size_t max_fake_count;
 
-void Solve(std::vector<std::string> &s1, std::vector<std::string> &s2, 
-  std::map<std::string, size_t> &s1_count, std::map<std::string, size_t> &s2_count, 
-  size_t index, size_t fake_count) {
-
-  if (index == s1.size()) {
-    max_fake_count = std::max(fake_count, max_fake_count);
+void Fill(const std::string &s, size_t index, size_t answer, std::vector<size_t> &solutions) {
+  if (index == s.size()) {
+    solutions.push_back(answer);
     return;
   }
-
-  Solve(s1, s2, s1_count, s2_count, index + 1, fake_count);
-
-  if (s1_count[s1[index]] > 1 && s2_count[s2[index]] > 1) {
-    //std::cout << i << " " << s1[i] << " " << s2[i] << "\n";
-    --s1_count[s1[index]];
-    --s2_count[s2[index]];
-    Solve(s1, s2, s1_count, s2_count, index + 1, fake_count + 1);
-    ++s1_count[s1[index]];
-    ++s2_count[s2[index]];
+  if (s[index] != '?') {
+    Fill(s, index + 1, (answer * 10) + s[index] - '0', solutions);
+  } else {
+    for (size_t i = 0; i <= 9; ++i) {
+      Fill(s, index + 1, answer * 10 + i, solutions);
+    }
   }
 }
 
@@ -33,18 +25,26 @@ int main() {
   size_t T;
   std::cin >> T;
   for (size_t t = 1; t <= T; ++t) {
-    size_t N;
-    std::cin >> N;
-    std::vector<std::string> s1(N), s2(N);
-    std::map<std::string, size_t> s1_count, s2_count;
-    for (size_t i = 0; i < N; ++i) {
-      std::cin >> s1[i] >> s2[i];
-      ++s1_count[s1[i]];
-      ++s2_count[s2[i]];
+    std::string s1, s2;
+    std::cin >> s1 >> s2;
+    std::vector<size_t> s1_solutions, s2_solutions;
+    Fill(s1, 0, 0, s1_solutions);
+    Fill(s2, 0, 0, s2_solutions);
+
+    size_t min_difference = 2e9;
+    long long s1_ans, s2_ans;
+
+    for (long long n1 : s1_solutions) {
+      for (long long n2 : s2_solutions) {
+        if (std::abs(n1 - n2) < min_difference) {
+          min_difference = std::abs(n1 - n2);
+          s1_ans = n1;
+          s2_ans = n2;
+        }
+      }
     }
-    max_fake_count = 0;
-    Solve(s1, s2, s1_count, s2_count, 0, 0);
-    std::cout << "Case #" << t << ": " << max_fake_count << "\n";
+
+    std::cout << "Case #" << t << ": " << std::setfill('0') << std::setw(s1.length()) << s1_ans << " " << std::setfill('0') << std::setw(s1.length()) << s2_ans << "\n";
   }
   return 0;
 }
